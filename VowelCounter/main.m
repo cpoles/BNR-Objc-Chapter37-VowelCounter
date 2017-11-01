ef223d7bf3e5d7909d2905f8557f4ef4fadc15fa
krustybr <carlos.poles@griffithuni.edu.au>
2017-11-01 20:55:46 +1000

    Initial Commit

---
+++ VowelCounter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VowelCounter
+//
+//  Created by Carlos Poles on 1/11/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
